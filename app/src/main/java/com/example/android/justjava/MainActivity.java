cc29470148df955e70253c70dc6925cedf73e17c
purnchndr <purnachandrapratapnishad@gmail.com>
2020-12-30 22:02:11 +0530

    Add quantity update function to increase or decrease.

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -25,7 +25,10 @@ import android.widget.TextView;
  * This app displays an order form to order coffee.
  */
 import androidx.appcompat.app.AppCompatActivity;
-public class MainActivity extends AppCompatActivity {
+
+import java.text.NumberFormat;
+
+public class MainActivity<var> extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,15 +39,41 @@ public class MainActivity extends AppCompatActivity {
     /**
      * This method is called when the order button is clicked.
      */
+    int  priceOfCoffee = 5,quantity = 0;
+
     public void submitOrder(View view) {
-        display("Chandan");
+        display(quantity);
+        displayPrice(quantity * priceOfCoffee);
     }
+    public void increment(View view){
+        quantity += 1;
+
+        display(quantity);
+        displayPrice(quantity * priceOfCoffee);
+    }
+
+    public void decrement(View view){
+        quantity -= 1;
+        if (quantity < 0){
+            quantity = 0;
+        }
+        display(quantity);
+        displayPrice(quantity * priceOfCoffee);
+    }
+
 
     /**
      * This method displays the given quantity value on the screen.
      */
-    private void display(String number) {
+    private void display(int number) {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
         quantityTextView.setText("" + number);
+    }
+    /**
+     * This method displays the given price on the screen.
+     */
+    private void displayPrice(int number) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
     }
 }
